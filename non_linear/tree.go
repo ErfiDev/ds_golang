f0f2c60398ad92b9792be6058adb2135bdc57967
erfanhanifezade <erfanhanifezade@gmail.com>
2023-02-08 23:06:59 +0330

    tree ds

---
+++ non_linear/tree.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+	In computer science, a tree is a widely used abstract
+	data type that represents a hierarchical tree structure
+	with a set of connected nodes. Each node in the tree
+	can be connected to many children (depending on the type
+	of tree), but must be connected to exactly one parent,
+	except for the root node, which has no parent.
+*/
+
+func main() {
+
+}
